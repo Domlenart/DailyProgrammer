401873dc7897c957138b5c4303e0cf1c4ecd4034
Dominik Lenart <dl7cz@virginia.edu>
2018-02-17 23:01:06 -0500

    Add scroll to treeview

--- app/interface.py
+++ app/interface.py
@@ -1,7 +1,10 @@
 import operator
 import tkinter as tk
+import webbrowser
+
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
+
 
 from app import file_tasks
 
@@ -9,8 +12,11 @@ from app import file_tasks
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title('Daily Programmer')
 
-        self.resizable(False, False)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+
         self.make_and_configure_tree()
         self.get_challenges()
         self.add_buttons()
@@ -18,31 +24,45 @@ class MainWindow(tk.Tk):
         self.add_textfield()
 
     def make_and_configure_tree(self):
-        self.tree = ttk.Treeview(self)
-        self.tree.configure(columns=['id', 'diff'], show='headings')
+        self.treeframe = tk.Frame()
+        self.treeframe.grid(row=0, column=0, sticky=tk.N+tk.S)
+
+        self.tree = ttk.Treeview(self.treeframe)
+        self.treescroll = ttk.Scrollbar(self.treeframe, command=self.tree.yview)
+        self.tree.pack(side='left', fill='y')
+        self.treescroll.pack(side='right', fill='y')
+
+        self.tree.configure(columns=['id', 'diff'], show='headings', yscrollcommand=self.treescroll.set)
+
         self.tree.heading('id', text='#', anchor=tk.CENTER, command=self.sort_by_id)
         self.tree.column('id', width=60, anchor=tk.CENTER)
         self.tree.heading('diff', text='Difficulty')
+
         self.tree.tag_configure('not_completed', background='yellow')
         self.tree.tag_configure('completed', background='green')
+
         self.tree.bind('<ButtonRelease-1>', self.display_challenge_text)
-        self.tree.grid(sticky=tk.N+tk.S)
+        self.tree.bind('<Double-Button-1>', self.open_reddit_thread)
 
     def add_combobox(self):
-        self.combobox = ttk.Combobox(values='easy intermediate difficult/hard weekly')
+        self.start_var = tk.StringVar()
+        self.start_var.set('Pick difficulty')
+        self.combobox = ttk.Combobox(textvariable=self.start_var, values='easy intermediate difficult/hard weekly',
+                                     state='readonly')
         self.combobox.bind('<<ComboboxSelected>>', self.display_diff)
-        self.combobox.grid()
+        self.combobox.grid(row=1, column=0)
 
     def add_buttons(self):
-        self.complete_button = ttk.Button(text='Complete', command=self.complete_challenge).grid()
-        self.update_button = ttk.Button(text='Update', command=self.update_challenges).grid()
+        self.update_button = ttk.Button(text='Update', command=self.update_challenges)
+        self.update_button.grid(row=2, column=0)
 
     def add_textfield(self):
-        self.textvar = tk.StringVar(value='Challenge text will appear here')
+        self.textvar = tk.StringVar()
+        self.textvar.set('Pick a challenge to see its text here')
         self.textfield = ScrolledText(wrap=tk.WORD, state='normal')
-        self.textfield.insert(tk.END, 'Challenge text will display here')
+        self.textfield.insert(tk.END, self.textvar.get())
         self.textfield.configure(state='disabled')
-        self.textfield.grid(column=1, row=0)
+        self.textfield.grid(column=1, row=0, sticky=(tk.E, tk.W, tk.N, tk.S), rowspan=3)
 
     def get_challenges(self):
         for challenge in file_tasks.get_challenge_status():
@@ -54,10 +74,8 @@ class MainWindow(tk.Tk):
             if challenge.tested:
                 tested = 'tested'
 
-            self.tree.insert('', 0, text='', values=[challenge.id, challenge.difficulty, challenge.text], tags=(completion, tested))
-
-    def complete_challenge(self):
-        print(self.tree.item(self.tree.focus(), tags=('completed', 'tested')))
+            self.tree.insert('', 0, text='', values=[challenge.id, challenge.difficulty, challenge.text],
+                             tags=(completion, tested))
 
     def update_challenges(self):
         self.tree.delete(*self.tree.get_children())
@@ -90,6 +108,8 @@ class MainWindow(tk.Tk):
         self.textfield.insert('1.0', f'{self.tree.item(self.tree.selection())["values"][2]}')
         self.textfield.configure(state='disabled')
 
+    def open_reddit_thread(self, _):
+        raise NotImplementedError
 
 if __name__ == '__main__':
     MainWindow().mainloop()
